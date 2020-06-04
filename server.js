9da826bb6bb737fecf3ac17cf472970b9e232d13
bkorkos <bar.korkos@intel.com>
2020-06-04 23:15:10 +0300

    fgdhf

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(''+__dirname + '/dist/app'));
+app.use(express.static(__dirname + 'dist/app'));
 
-app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-
-// app.get('/*', function(req,res) {
-    
-// res.sendFile(path.join('/side/'+__dirname+'/index.html'));
+// app.get('*', (req, res) => {
+//     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 // });
+
+app.get('*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
